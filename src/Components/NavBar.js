8cba9866c1b562f5d02a17d6b3870aad0a8ddd62
Andisiwe Nonkwenkwe <andisiwenonkwenkwe30@gmail.com>
2024-02-23 16:13:02 +0200

    changes

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,12 +1,15 @@
 import React, { useState } from 'react';
 import './Navbar.css';
 
-
 function Navbar() {
   const [isOpen, setIsOpen] = useState(false);
 
   const toggleMenu = () => {
     setIsOpen(!isOpen);
+  };
+
+  const handleLinkClick = () => {
+    setIsOpen(false); // Close the menu when a link is clicked
   };
 
   return (
@@ -15,11 +18,11 @@ function Navbar() {
         <div className="navbar-logo"><span>A.N</span></div>
         <div className={`navbar-links ${isOpen ? 'active' : ''}`}>
           <ul>
-            <li><a href="#home">Home</a></li>
-            <li><a href="#about">About</a></li>
-            <li><a href="#projects">Projects</a></li>
-            <li><a href="#testimonies">Testimonies</a></li>
-            <li><a href="#contact">Contact</a></li>
+            <li><a href="#home" onClick={handleLinkClick}>Home</a></li>
+            <li><a href="#about" onClick={handleLinkClick}>About</a></li>
+            <li><a href="#projects" onClick={handleLinkClick}>Projects</a></li>
+            <li><a href="#testimonies" onClick={handleLinkClick}>Testimonies</a></li>
+            <li><a href="#contact" onClick={handleLinkClick}>Contact</a></li>
           </ul>
         </div>
         <div className="hamburger-menu" onClick={toggleMenu}>
